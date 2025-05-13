01cdbee54b5f4cc96e2ef4eeda528e38aa94f3f3
Mariam Hafez <mah9994@nyu.edu>
2025-05-13 23:43:52 +0400

    portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+// Simple fade effect for transitions if needed
+window.onload = () => {
+    document.body.style.opacity = 0;
+    setTimeout(() => {
+        document.body.style.transition = "opacity 1s";
+        document.body.style.opacity = 1;
+    }, 100);
+};
